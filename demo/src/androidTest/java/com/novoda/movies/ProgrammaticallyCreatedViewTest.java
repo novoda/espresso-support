586b382d65a9653c9571b6978972ea1504adea7c
Ataul Munim <hello@ataulm.com>
2017-11-08 22:41:46 +0000

    Add example test with ViewCreator

---
+++ demo/src/androidTest/java/com/novoda/movies/ProgrammaticallyCreatedViewTest.java
@@ -0,0 +1,49 @@
+package com.novoda.movies;
+
+import android.content.Context;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.novoda.espresso.ViewCreator;
+import com.novoda.espresso.ViewTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ProgrammaticallyCreatedViewTest {
+
+    private static final String TEXT = "Hello world!";
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ViewTestRule<TextView> viewTestRule = new ViewTestRule<>(new ViewCreator<TextView>() {
+        @Override
+        public TextView createView(Context context, ViewGroup parentView) {
+            TextView textView = new TextView(context);
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+            textView.setLayoutParams(layoutParams);
+            textView.setText(TEXT);
+            return textView;
+        }
+    });
+
+    @Test
+    public void createdViewIsDisplayed() {
+        onView(withText(TEXT)).check(matches(isDisplayed()));
+    }
+}
