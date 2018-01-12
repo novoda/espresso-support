851b1c6a514f8134a6c09358ec9988546e9da196
Ataul Munim <hello@ataulm.com>
2018-01-12 14:24:32 +0000

    Add polling and waiting instead of naive wait for TalkBack toggling

--- core/src/main/java/com/novoda/espresso/TalkBackTestRule.java
+++ core/src/main/java/com/novoda/espresso/TalkBackTestRule.java
@@ -1,10 +1,15 @@
 package com.novoda.espresso;
 
+import android.content.Context;
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.view.accessibility.AccessibilityManager;
 
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
+import static android.accessibilityservice.AccessibilityServiceInfo.FEEDBACK_SPOKEN;
 
 public class TalkBackTestRule implements TestRule {
 
@@ -15,10 +20,11 @@ public class TalkBackTestRule implements TestRule {
 
     private static class TalkBackStatement extends Statement {
 
-        private static final int DEFAULT_DELAY_MILLIS = 1500;
+        private static final int SLEEP_DELAY_MILLIS = 10;
 
-        private final TalkBackStateSettingRequester talkBackStateSettingRequester = new TalkBackStateSettingRequester(0);
         private final Statement baseStatement;
+        private final TalkBackStateSettingRequester talkBackStateSettingRequester = new TalkBackStateSettingRequester(0);
+        private final AccessibilityManager a11yManager = (AccessibilityManager) InstrumentationRegistry.getInstrumentation().getContext().getSystemService(Context.ACCESSIBILITY_SERVICE);
 
         TalkBackStatement(Statement baseStatement) {
             this.baseStatement = baseStatement;
@@ -27,16 +33,32 @@ public class TalkBackTestRule implements TestRule {
         @Override
         public void evaluate() throws Throwable {
             talkBackStateSettingRequester.requestEnableTalkBack();
-            sleepToAllowTalkBackServiceToChangeState();
+            sleepUntilTalkBackIsEnabled();
 
             baseStatement.evaluate();
 
             talkBackStateSettingRequester.requestDisableTalkBack();
-            sleepToAllowTalkBackServiceToChangeState();
+            sleepUntilTalkBackIsDisabled();
         }
 
-        private void sleepToAllowTalkBackServiceToChangeState() {
-            SystemClock.sleep(DEFAULT_DELAY_MILLIS);
+        private void sleepUntilTalkBackIsEnabled() {
+            while (talkBackDisabled()) {
+                SystemClock.sleep(SLEEP_DELAY_MILLIS);
+            }
+        }
+
+        private void sleepUntilTalkBackIsDisabled() {
+            while (talkBackEnabled()) {
+                SystemClock.sleep(SLEEP_DELAY_MILLIS);
+            }
+        }
+
+        private boolean talkBackEnabled() {
+            return !talkBackDisabled();
+        }
+
+        private boolean talkBackDisabled() {
+            return a11yManager.getEnabledAccessibilityServiceList(FEEDBACK_SPOKEN).isEmpty();
         }
     }
 }
