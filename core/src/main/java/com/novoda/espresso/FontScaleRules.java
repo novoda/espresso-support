afa7fbaa2290f455ad7f30563778e1252b5f2360
Ataul Munim <hello@ataulm.com>
2018-02-09 15:02:27 +0000

    Move TargetApi annotation to top of class instead of for all methods

--- core/src/main/java/com/novoda/espresso/FontScaleRules.java
+++ core/src/main/java/com/novoda/espresso/FontScaleRules.java
@@ -7,29 +7,25 @@ import android.support.test.InstrumentationRegistry;
 
 import org.junit.rules.TestRule;
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class FontScaleRules {
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static TestRule smallFontScaleTestRule() {
         return new FontScaleTestRule(createFontScaleSetting(), FontScale.SMALL);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static TestRule normalFontScaleTestRule() {
         return new FontScaleTestRule(createFontScaleSetting(), FontScale.NORMAL);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static TestRule largeFontScaleTestRule() {
         return new FontScaleTestRule(createFontScaleSetting(), FontScale.LARGE);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static TestRule hugeFontScaleTestRule() {
         return new FontScaleTestRule(createFontScaleSetting(), FontScale.HUGE);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private static FontScaleSetting createFontScaleSetting() {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         return new FontScaleSetting(instrumentation.getUiAutomation(), instrumentation.getTargetContext().getResources());
